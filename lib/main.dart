302333bc49a8ae23572ef80021b05d17b5c87701
이정훈 <gns14585@naver.com>
2024-03-17 10:01:20 +0900

    스크롤

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          title: Text("금호동3가", style: TextStyle(color: Colors.white),),
-          actions: [ 
+          title: Text("금호동3가", style: TextStyle(color: Colors.white)),
+          actions: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Icon(Icons.search),
@@ -28,62 +27,116 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Icon(Icons.home),
             ),
-          ] ,
+          ],
         ),
-
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset("assets/KakaoTalk_20240216_015701309.png",
-                width: 100,
-                height: 120,
-                fit: BoxFit.cover
-                ),
-            ),
-              SizedBox(width: 10,), // 이미지 옆 여백주기
-
-              Expanded(
-               child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)',
-                    style: TextStyle(fontSize: 18),),
-                    Text('금호동3가', style: TextStyle(color: Colors.grey),),
-                    Text('7000원', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text("4")
-                      ],
-                    )
-                  ],
-                ),
-              )
-              )
-            ],
-          ),
+        body: ListView(
+          children: [
+            listItem(), 
+            listItem2(), 
+            listItem(), 
+            listItem2(),
+            listItem(), 
+            listItem2(),
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
-          height: 50,
+            height: 50,
           child: Container(
-            child: (
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.book)
-                ],
-              )
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget listItem() {
+    // 리스트 아이템을 만드는 메서드
+    return Container(
+      height: 150,
+      padding: EdgeInsets.all(10),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              "assets/KakaoTalk_20240216_015701309.png",
+              width: 100,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 10), // 이미지 옆 여백주기
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text('금호동3가', style: TextStyle(color: Colors.grey)),
+                  Text('7,000원',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [Icon(Icons.favorite), Text("2")],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget listItem2() {
+    return Container(
+      height: 150,
+      padding: EdgeInsets.all(10),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              "assets/9941A1385B99240D2E.png",
+              width: 100,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("김영한의 스프링부트강의", style: TextStyle(fontSize: 18),),
+                  Text("금호동3가", style: TextStyle(color: Colors.grey),),
+                  Text("49,000원", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(Icons.favorite),
+                      Text("20")
+                    ],
+                  )
+                  ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
